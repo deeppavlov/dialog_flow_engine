9c0b725aaf4301b9da6b877b0f44b0afb7f7ef2c
dimakarp1996 <dimakarp1996@yandex.ru>
2021-12-08 10:54:02 +0300

    Update responses.py

--- dff/responses.py
+++ dff/responses.py
@@ -1,7 +1,7 @@
 """
 Responses
 ---------------------------
-
+Functions that work with responses.
 """
 import random
 from .core.context import Context
@@ -9,6 +9,10 @@ from .core.actor import Actor
 
 
 def choice(responses: list):
+    """
+    Function wrapper that takes the list of responses as an input,
+    and returns handler which outputs a response randomly chosen from that list.
+    """
     def choice_response_handler(ctx: Context, actor: Actor, *args, **kwargs):
         return random.choice(responses)
 
